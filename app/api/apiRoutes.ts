4aaf5a7491d0276dc65e50e99181df89fcb7899a
LaurelQuadras <laurelquadras@gmail.com>
2024-04-21 17:36:45 +0200

    Removing headers

--- app/api/apiRoutes.ts
+++ app/api/apiRoutes.ts
@@ -21,10 +21,6 @@ export const postSectionsApi = async (name: string, description: string) => {
       "https://gamewithcolors.online/exams/subjects",
       {
         method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
         body: JSON.stringify({
           subject_name: name,
           subject_description: description,
@@ -50,10 +46,6 @@ export const getSectionApi = async (): Promise<string[]> => {
       "https://gamewithcolors.online/exams/subjects",
       {
         method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
       }
     );
     const result: GetSectionApiType[] = await response.json();
